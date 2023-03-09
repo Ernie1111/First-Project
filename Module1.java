f4b0502d4279b693970d812541d70634af1d6d43
Ernesto Huerta <121065240+Ernie1111@users.noreply.github.com>
2023-03-09 13:02:14 -0800

    Create Module1.java
    My first project for school and for myself

---
+++ Module1.java
@@ -0,0 +1,29 @@
+
+public class Module1 {
+
+	public static void main(String[] args) {
+		int numZip;
+		//numZip will be used later on in the program for the zipcode
+		numZip = 28104;
+		
+		//Character First Name
+		System.out.print("First Name: ");
+		System.out.println("Cloud");
+		
+		// Character Last Name
+		System.out.print("Last Name: ");
+		System.out.println("Strife");
+		
+		//Characters Street Address
+		System.out.print("Street Address: ");
+		System.out.println("702 Andover St. ");
+		System.out.print("City: ");
+		System.out.println("Matthews, NC ");
+		System.out.print("Zipcode: ");
+		System.out.print(numZip);
+		
+	
+
+	}
+
+}
